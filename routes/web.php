8e53b28a45fa35581efcf3714c07cbd820dcf01a
Abinaya441 <kiko4801@gmail.com>
2022-11-26 23:28:00 +0700

    Inisialisasi routing

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
+use App\Http\Controllers\BukuController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\KaryawanController;
+use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\PenerbitController;
+use App\Http\Controllers\PenulisController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +20,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [LoginController::class, 'index'])->middleware('guest');
+// Memberikan nama 'login' ke rute login agar user yang belum login dapat diarahkan ke rute ini
+Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
+Route::post('/login', [LoginController::class, 'authenticate'])->middleware('guest');
+// Rute untuk logout
+Route::post('/logout', [LoginController::class, 'logout'])->middleware('auth');
+
+Route::get('/home', [HomeController::class, 'index'])->middleware('auth');
+Route::resource('/karyawan', KaryawanController::class)->middleware('admin');
+Route::resource('/buku', BukuController::class)->middleware('user');
+Route::resource('/buku/kategori', KategoriController::class)->middleware('admin');
+Route::resource('/buku/penulis', PenulisController::class)->middleware('user');
+Route::resource('/buku/penerbit', PenerbitController::class)->middleware('user');
