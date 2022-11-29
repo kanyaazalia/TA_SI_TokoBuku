40fcf8f25909682b5cb5f0b44ae72dddea30a213
Kanya Azalia Andriyani <kanyaazalia@gmail.com>
2022-11-29 08:48:20 +0700

    Penambahan Gate
    Menambahkan gate untuk autentikasi role pada resource controller

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Karyawan;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Gate untuk identifikasi admin atau user di resource controller
+        Gate::define('admin', function (Karyawan $karyawan){
+            return $karyawan->admin >= 1;
+        });
+
+        Gate::define('user', function (Karyawan $karyawan){
+            return $karyawan->admin >= 0;
+        });
     }
 }
